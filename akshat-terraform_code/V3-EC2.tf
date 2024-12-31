222c05ae71d989069c12f4132f45a00e7b0743f6
InnocentShiva <singh.akshat.solanki@gmail.com>
2024-12-31 21:19:34 +0530

    Terraform code to create EC2 with Security group and with VPC

---
+++ akshat-terraform_code/V3-EC2.tf
@@ -0,0 +1,102 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0fd05997b4dff7aac"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.custom-demo-public-subnet-01.id
+    key_name = "git-server-key"
+    //security_groups = ["${aws_security_group.demo-sg.id}"]
+    vpc_security_group_ids = [aws_security_group.demo-sg.id]         
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.custom-demo-vpc.id
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "demo-sg_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "demo-sg_ipv6" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv6         = "::/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+  security_group_id = aws_security_group.demo-sg.id
+  cidr_ipv6         = "::/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+resource "aws_vpc" "custom-demo-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "custom-demo-vpc"
+  }
+}
+
+resource "aws_subnet" "custom-demo-public-subnet-01" {
+  vpc_id = aws_vpc.custom-demo-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "custom-demo-public-subnet-01"
+  }
+}
+
+resource "aws_subnet" "custom-demo-public-subnet-02" {
+  vpc_id = aws_vpc.custom-demo-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "custom-demo-public-subnet-02"
+  }
+}
+
+resource "aws_internet_gateway" "custom-demo-igw" {
+  vpc_id=aws_vpc.custom-demo-vpc.id
+  tags = {
+    Name = "custom-demo-igw"
+  }
+}
+
+resource "aws_route_table" "custom-demo-public-rt" {
+  vpc_id = aws_vpc.custom-demo-vpc.id
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.custom-demo-igw.id
+  }
+}
+
+resource "aws_route_table_association" "custom-demo-rta-public-subnet-01" {
+  subnet_id = aws_subnet.custom-demo-public-subnet-01.id
+  route_table_id = aws_route_table.custom-demo-public-rt.id
+}
+
+resource "aws_route_table_association" "custom-demo-rta-public-subnet-02" {
+  subnet_id = aws_subnet.custom-demo-public-subnet-02.id
+  route_table_id = aws_route_table.custom-demo-public-rt.id
+}
